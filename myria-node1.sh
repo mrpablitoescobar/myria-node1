fb66bc45625a0718bf19710ea1c354d316ce7ee4
mrpablitoescobar <42651895+mrpablitoescobar@users.noreply.github.com>
2025-05-22 20:26:31 +0100

    Update myria-node1.sh

--- myria-node1.sh
+++ myria-node1.sh
@@ -1,21 +1,26 @@
 #!/bin/bash
+
 if [ $EUID != 0 ]; then
     sudo "$0" "$@"
     exit $?
 fi
-echo "Checking system requirements , please wait."
+
+echo "Checking system requirements, please wait."
 CORE=$(grep -c ^processor /proc/cpuinfo)
 MEM=$(grep -oP '^MemTotal:\s+\K\d+' /proc/meminfo)
-if (( $CORE < 2 )); then
+
+if (( CORE < 2 )); then
   echo ">>>[ERROR] Your CPU cores must be greater than 2"
   exit 1
 fi
+
 if (( MEM < 2621440 )); then
   echo ">>>[ERROR] Your RAM must be greater than 3GB"
   exit 1
 fi
 
-echo ">>>[INFO] Installing requirements packages"
+echo ">>>[INFO] Installing required packages"
+sudo apt update
 sudo apt install curl -y
 
 echo ">>>[INFO] Downloading Myria Node Software package"
@@ -29,6 +34,8 @@ SERVICE_NAME="myria-node1"
 SERVICE_PATH="$MYRIA_DIR/myria-node"
 
 echo ">>>[INFO] Installing Myria Node Software"
+
+# check if service exists
 service_exists() {
     local n=$1
     if [[ $(systemctl list-units --all -t service --full --no-legend "$n.service" | awk '{print $1}') == "$n.service" ]]; then
@@ -51,24 +58,29 @@ sudo wget -O /usr/local/bin/myria-node1 "https://downloads-builds.myria.com/node
 sudo chmod +x /usr/local/bin/myria-node1
 
 sudo mkdir -p /etc/sysconfig
+
 cat > /etc/sysconfig/myria-node1-service << EOF
-# Add environment variables here if needed, e.g.:
-# MYRIA_ARG1="--port 4001"
+# MYRIA_ARG1="--port 4001" # You can define custom args here
 EOF
 
 sudo tee /etc/systemd/system/${SERVICE_NAME}.service > /dev/null << EOF
 [Unit]
 Description=$DESCRIPTION
 After=network.target
+
 [Service]
 EnvironmentFile=/etc/sysconfig/myria-node1-service
 ExecStart=$SERVICE_PATH \$MYRIA_ARG1
 Restart=on-failure
+
 [Install]
 WantedBy=multi-user.target
 EOF
 
-echo ">>>[INFO] Enabling service"
+echo ">>>[INFO] Enabling and starting $SERVICE_NAME"
 sudo systemctl daemon-reload
 sudo systemctl enable $SERVICE_NAME
-echo ">>>[INFO] Install Completed for $SERVICE_NAME!"
+sudo systemctl start $SERVICE_NAME
+
+echo ">>>[INFO] Installation completed for $SERVICE_NAME!"
+echo ">>>[INFO] Use 'myria-node1 --help' for usage instructions."
